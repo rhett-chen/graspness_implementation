437131ff6eb2b0f554fac05b1e8187f3c226f823
rhett-chen <2206571319@qq.com>
2023-03-14 11:00:20 +0800

    fix bug

--- dataset/simplify_dataset.py
+++ dataset/simplify_dataset.py
@@ -17,18 +17,18 @@ def simplify_grasp_labels(root, save_path):
     for i in obj_names:
         print('\nsimplifying object {}:'.format(i))
         label = np.load(os.path.join(root, 'grasp_label', '{}_labels.npz'.format(str(i).zfill(3))))
-        point_num = len(label['points'])
+        # point_num = len(label['points'])
         print('original shape:               ', label['points'].shape, label['offsets'].shape, label['scores'].shape)
-        if point_num > 4820:
-            idxs = np.random.choice(point_num, 4820, False)
-            points = label['points'][idxs]
-            offsets = label['offsets'][idxs]
-            scores = label['scores'][idxs]
-            print('Warning!!!  down sample object {}'.format(i))
-        else:
-            points = label['points']
-            scores = label['scores']
-            offsets = label['offsets']
+        # if point_num > 4820:
+        #     idxs = np.random.choice(point_num, 4820, False)
+        #     points = label['points'][idxs]
+        #     offsets = label['offsets'][idxs]
+        #     scores = label['scores'][idxs]
+        #     print('Warning!!!  down sample object {}'.format(i))
+        # else:
+        points = label['points']
+        scores = label['scores']
+        offsets = label['offsets']
         width = offsets[:, :, :, :, 2]
         print('after simplify, offset shape: ', points.shape, scores.shape, width.shape)
         np.savez(os.path.join(save_path, '{}_labels.npz'.format(str(i).zfill(3))),
